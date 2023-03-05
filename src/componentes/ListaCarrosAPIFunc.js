cdcbe9c8f5628be364a203bf79f0d3e598bb7bc4
Juhdev2022 <98654148+Juhdev2022@users.noreply.github.com>
2023-03-05 16:44:59 -0300

    API Nodejs/reactjs

--- src/componentes/ListaCarrosAPIFunc.js
+++ src/componentes/ListaCarrosAPIFunc.js
@@ -1,7 +1,7 @@
 import React,{useEffect, useState} from "react";
 import axios from 'axios'
 
-export default function ListaCarrosAPIFunc(){
+export default function ListaCarros(){
 
    const [carros,setCarros]=useState([])
 
